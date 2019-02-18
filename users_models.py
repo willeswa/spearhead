7b09a1f9862a7515771f9fc75116a90cdc21c48b
Willies Wanjala <gwiliez@gmail.com>
2019-02-18 11:53:43 +0300

    [finish #164042318] project layout

---
+++ users_models.py
@@ -0,0 +1,19 @@
+""" This module contains classes for the project """
+
+
+class UserBaseClass:
+    """ Defines methods common to all users """
+    pass
+
+
+class Moderator(UserBaseClass):
+    """ Defines  """
+    pass
+
+
+class Admin(Moderator):
+    pass
+
+
+class Comments:
+    pass
