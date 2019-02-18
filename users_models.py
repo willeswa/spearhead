63336427409bb6a73802fad7050cf57e2d790ab0
hezronkimutai <hezronchelimo.hc@gmail.com>
2019-02-18 13:04:50 +0300

    [Starts #164043044] Create comment class

--- users_models.py
+++ users_models.py
@@ -1,6 +1,6 @@
 """ This module contains classes for the project """
 
-
+comments = []
 class UserBaseClass:
     """ Defines methods common to all users """
     pass
@@ -16,4 +16,27 @@ class Admin(Moderator):
 
 
 class Comments:
-    pass
+    def __init__(self, message, author, timestamp, replying_to):
+        self.message = message
+        self.author = author
+        self.timestamp = timestamp
+        self.relying_to = replying_to
+
+    def create_comment(self):
+        self.comments.append({
+            "comment_id":len(self.comments)
+            "message": self.message,
+            "author": self.author
+            "timestamp": self.timestamp
+            "replying_to": self.replying_to
+        })
+
+    def edit_comment(self, comment_id):
+        for comment in self.comments:
+            if comment_id in comment.values():
+                comment[message] = self.message
+
+    def delete_comment(self, comment_id):
+        for comment in self.comments:
+            if comment_id in comment.values():
+                del self.comments[comment]:
